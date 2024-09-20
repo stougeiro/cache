08f9ce3d7147b7e01d65d7f0b97888792d538415
STougeiro <sidneytougeiro@gmail.com>
2024-09-20 00:40:35 -0300

    Adding a Service Provider

---
+++ src/CacheServiceProvider.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+    namespace STDW\Cache;
+
+    use STDW\Contract\ServiceProviderAbstracted;
+    use STDW\Cache\Contract\CacheInterface;
+    use STDW\Cache\Contract\CacheHandlerInterface;
+    use STDW\Cache\Cache;
+    use STDW\Cache\Handler\NullHandler;
+
+
+    class CacheServiceProvider extends ServiceProviderAbstracted
+    {
+        public function register(): void
+        {
+            $this->app->singleton(CacheInterface::class, Cache::class);
+            $this->app->singleton(CacheHandlerInterface::class, NullHandler::class);
+        }
+
+        public function boot(): void
+        {
+            $this->app->cache = $this->app->make(CacheInterface::class);
+        }
+
+        public function terminate(): void
+        {
+        }
+    }
